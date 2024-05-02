2b082f63113f46d530dbbbf84c5afcda7102da77
Vinicius Araujo <viniciusaraujoaj@gmail.com>
2024-05-02 15:50:51 -0300

    adicao do js e algumas modificacoes no CSS

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("oi");
+const btnCard = document.querySelector(".btn-card");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
+
+btnCard.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+});
